8fbed0b3c1a5962fedab95ca41e54c002b8872d7
IrgiAhmadzFhrezi <fahrezijr145@gmail.com>
2024-05-30 22:35:58 +0800

    test

--- uploadConfig.js
+++ uploadConfig.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "static"); // Menghapus spasi yang tidak perlu
+    cb(null, "/static/"); // Menghapus spasi yang tidak perlu
   },
   filename: (req, file, cb) => {
     cb(
