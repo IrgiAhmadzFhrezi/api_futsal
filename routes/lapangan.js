817e97a87136c5a5b4248703d763e23e62ee0839
IrgiAhmadzFhrezi <fahrezijr145@gmail.com>
2024-06-02 10:52:19 +0800

    new

--- routes/lapangan.js
+++ routes/lapangan.js
@@ -1,23 +1,21 @@
 const router = require("express").Router();
 const lapanganController = require("../controllers/lapanganController");
-const multer = require('multer')
+const uploadConfig = require("../uploadConfig");
+const fields = uploadConfig.upload.fields([
+  {
+    name: "gambar",
+    maxCount: 1,
+  },
+]);
 
-const gambar = multer.diskStorage({
-    filename: async function (req, file, cb) {
-        let ext = file.originalname.substring(
-            file.originalname.lastIndexOf("."),
-            file.originalname.length
-        )
-        cb(null, Date.now() + ext)
-    },
-    destination: async function (req, file, cb) {
-        cb(null, './static/')
-    }
-})
-
-const uploadImg = multer({ storage: gambar }).single("gambar")
-
-router.post('/create', uploadImg, controller.create)
+router.post("/create", fields, (req, res) => {
+  req.body.gambar = req.files.gambar[0].filename;
+  // console.log(req.body)
+  lapanganController
+    .create(req.body)
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
 router.put("/edit/:id", fields, (req, res) => {
   const gambar = uploadConfig.cekNull(req.files.gambar);
